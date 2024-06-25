b5204f365a132056382e68295120760108911081
abduvaliyev-1995 <75872152+abduvaliyev-1995@users.noreply.github.com>
2024-06-25 17:51:41 +0500

    Update urls.py

--- chat/urls.py
+++ chat/urls.py
@@ -19,6 +19,7 @@ schema_view = get_schema_view(
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
    ),
+    url='https://chatapi-5cbh.onrender.com/',
    public=True,
    permission_classes=(permissions.AllowAny,),
 )
