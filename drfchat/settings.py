9bf339eefe0e30e2fa4bb2665f40d3a2779dfeb9
abduvaliyev-1995 <75872152+abduvaliyev-1995@users.noreply.github.com>
2024-06-25 17:43:08 +0500

    Update settings.py

--- drfchat/settings.py
+++ drfchat/settings.py
@@ -59,7 +59,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     #'corsheaders.middleware.CorsMiddleware',
-    'drfchat.chat.middleware.cors.CorsMiddleware',
+    'chat.middleware.cors.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
