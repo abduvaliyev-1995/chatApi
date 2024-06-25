29e52017f35d0d0574e92131054b9a5526c44fe8
abduvaliyev-1995 <75872152+abduvaliyev-1995@users.noreply.github.com>
2024-06-25 17:39:33 +0500

    Add files via upload

---
+++ chat/middleware/cors.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.utils.deprecation import MiddlewareMixin
+
+
+class CorsMiddleware(MiddlewareMixin):
+    def process_response(self, request, response):
+        response["Acces-Control-Allow-Origin"] = "*"
+        return response
